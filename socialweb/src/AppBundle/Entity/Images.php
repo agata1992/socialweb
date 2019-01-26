3f097307e40e7ea363edbc1b97a40769a1849f1b
Agata Agata <agata.sawicka1992@interia.pl>
2019-01-26 16:25:01 +0100

    ''

--- socialweb/src/AppBundle/Entity/Images.php
+++ socialweb/src/AppBundle/Entity/Images.php
@@ -10,75 +10,75 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Images{
 	
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+	/**
+	 * @ORM\Id
+	 * @ORM\GeneratedValue
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
 	
 	/**
-    * @ORM\Column(name="album_id",type="integer")
-    * Doctrine\ORM\Mapping\GeneratedValue(strategy="AUTO")
-    * 
-    */
+	* @ORM\Column(name="album_id",type="integer")
+	* Doctrine\ORM\Mapping\GeneratedValue(strategy="AUTO")
+	* 
+	*/
 	private $album_id;
 	
 	/**
-    * @ORM\Column(name="image_name",type="string",length=255)
-    * Doctrine\ORM\Mapping\GeneratedValue(strategy="AUTO")
-    * 
-    */
+	* @ORM\Column(name="image_name",type="string",length=255)
+	* Doctrine\ORM\Mapping\GeneratedValue(strategy="AUTO")
+	* 
+	*/
 	private $image_name;
 	
 	/**
-    * @ORM\Column(name="description",type="string",length=255)
-    * Doctrine\ORM\Mapping\GeneratedValue(strategy="AUTO")
-    * 
-    */
+	* @ORM\Column(name="description",type="string",length=255)
+	* Doctrine\ORM\Mapping\GeneratedValue(strategy="AUTO")
+	* 
+	*/
 	private $description;
 	
 	/**
-    * @ORM\Column(name="add_date",type="date")
-    * Doctrine\ORM\Mapping\GeneratedValue(strategy="AUTO")
-    * 
-    */
+	* @ORM\Column(name="add_date",type="date")
+	* Doctrine\ORM\Mapping\GeneratedValue(strategy="AUTO")
+	* 
+	*/
 	private $add_date;
 	
 	public function getId(){
-        return $this->id;
-    }
+		return $this->id;
+	}
 	
 	public function getalbum_id(){
-        return $this->album_id;
-    }
+		return $this->album_id;
+	}
 	
 	public function getimage_name(){
-        return $this->image_name;
-    }
+		return $this->image_name;
+	}
 	
 	public function getdescription(){
-        return $this->description;
-    }
+		return $this->description;
+	}
 	
 	public function getadd_date(){
-        return $this->add_date;
-    }
+		return $this->add_date;
+	}
 	
 	public function setalbum_id($album_id){
-        $this->album_id = $album_id;
-    }
+		$this->album_id = $album_id;
+	}
 	
 	public function setimage_name($image_name){
-        $this->image_name = $image_name;
-    }
+		$this->image_name = $image_name;
+	}
 	
 	public function setdescription($description){
-        $this->description = $description;
-    }
+		$this->description = $description;
+	}
 	
 	public function setadd_date($add_date){
-        $this->add_date = $add_date;
-    }
+		$this->add_date = $add_date;
+	}
 }
 ?>
