481a9161e47da7d5959456e0d5688ba7ceacd7d5
Agata Agata <agata.sawicka1992@interia.pl>
2019-01-26 16:28:26 +0100

    ''

--- socialweb/src/AppBundle/Service/DBService.php
+++ socialweb/src/AppBundle/Service/DBService.php
@@ -11,28 +11,28 @@ use  AppBundle\Entity\Images;
 
 class DBService{
 	
-    protected $requestStack;
+	protected $requestStack;
 	protected $entityManager;
-    protected $user;
+	protected $user;
 	protected $user_data;
 	
 	public function __construct(RequestStack $requestStack,EntityManager $entityManager){
-	    $this->requestStack = $requestStack;
+		$this->requestStack = $requestStack;
 		$this->entityManager = $entityManager;
 	}	
 	
-    public function get_user_cookie(){
-	    $request = $this->requestStack->getCurrentRequest();
-	    $cookie = $request->cookies;
-	    $this->user = $cookie->get("user");
+	public function get_user_cookie(){
+		$request = $this->requestStack->getCurrentRequest();
+		$cookie = $request->cookies;
+		$this->user = $cookie->get("user");
 	}	
 
-    public function get_user_data(){
+	public function get_user_data(){
 		$this->get_user_cookie();
-        $this->user_data = $this->entityManager
-        ->getRepository(Users::class)->findOneBy(['email' => $this->user]);
-	    
-	    $this->user_data_array = array($this->user_data->getid(),$this->user_data->getname(),$this->user_data->getsurname(),
+		$this->user_data = $this->entityManager
+		->getRepository(Users::class)->findOneBy(['email' => $this->user]);
+		
+		$this->user_data_array = array($this->user_data->getid(),$this->user_data->getname(),$this->user_data->getsurname(),
 		$this->user_data->getemail(),$this->user_data->getpassword(),$this->user_data->getsalt(),$this->user_data->getcity(),
 		$this->user_data->getbirthdate(),$this->user_data->getgender(),$this->user_data->getprofile_img());
 		
@@ -41,38 +41,38 @@ class DBService{
 	
 	public function get_user_albums(){
 		$this->get_user_data();
-	    $albums = $this->entityManager
-        ->getRepository(Albums::class)->findBy(['user_id' => $this->user_data_array[0]]);
+		$albums = $this->entityManager
+		->getRepository(Albums::class)->findBy(['user_id' => $this->user_data_array[0]]);
 		return $albums;
 	}
 
-    public function get_user_album_by_id($album_id){
+	public function get_user_album_by_id($album_id){
 		$this->get_user_data();
-	    $album = $this->entityManager
-        ->getRepository(Albums::class)->findOneBy(['user_id' => $this->user_data_array[0],'id'=>$album_id]);
+		$album = $this->entityManager
+		->getRepository(Albums::class)->findOneBy(['user_id' => $this->user_data_array[0],'id'=>$album_id]);
 		
 		return $album;
 	}
 	
 	public function get_images_by_album_id($album_id){
-	    $this->get_user_data();
-	    $images = $this->entityManager
-        ->getRepository(Images::class)->findBy(['album_id' => $album_id]);
+		$this->get_user_data();
+		$images = $this->entityManager
+		->getRepository(Images::class)->findBy(['album_id' => $album_id]);
 		return $images;
 	
 	}
 	
 	public function get_image_by_image_id($image_id,$album_id){
-	    $this->get_user_data();
-	    $image = $this->entityManager
-        ->getRepository(Images::class)->findOneBy(['id'=>$image_id,'album_id' => $album_id]);
+		$this->get_user_data();
+		$image = $this->entityManager
+		->getRepository(Images::class)->findOneBy(['id'=>$image_id,'album_id' => $album_id]);
 		return $image;
 	}
 	
 	public function save_user_image_info($image_name,$album_id){
 		$image = new Images();
 		$image->setalbum_id($album_id);
-        $image->setimage_name($image_name);
+		$image->setimage_name($image_name);
 		$image->setdescription('');
 		$image->setadd_date(new \DateTime(date("Y-m-d")));
 		$this->entityManager->persist($image);
@@ -88,7 +88,7 @@ class DBService{
 	public function check_album_exists($album_name){
 		$this->get_user_data();
 		$album = $this->entityManager
-        ->getRepository(Albums::class)->findOneBy(['user_id' => $this->user_data_array[0],'album_name'=>$album_name]);
+		->getRepository(Albums::class)->findOneBy(['user_id' => $this->user_data_array[0],'album_name'=>$album_name]);
 		return $album;
 	}
 	
@@ -99,18 +99,18 @@ class DBService{
 	}
 	
 	public function add_album($album_name){
-	    $this->get_user_data();
+		$this->get_user_data();
 		$new_album = new Albums();
 		$new_album->setuser_id($this->user_data_array[0]);
 		$new_album->setalbum_name($album_name);
-	    $new_album->setalbum_access(0);
-        $this->entityManager->persist($new_album);
+		$new_album->setalbum_access(0);
+		$this->entityManager->persist($new_album);
 		$this->entityManager->flush();	
 	}
 	
 	public function getimages($album_id){
 		$images = $this->entityManager
-        ->getRepository(Images::class)->findBy(['album_id' => $album_id]);
+		->getRepository(Images::class)->findBy(['album_id' => $album_id]);
 		
 		return $images;
 	}
@@ -122,13 +122,13 @@ class DBService{
 	
 	public function remove_album($album){
 		$this->entityManager->remove($album);
-	    $this->entityManager->flush();
+		$this->entityManager->flush();
 	}
 	
 	
 	public function check_email($email){
 		$email = $this->entityManager
-        ->getRepository(Users::class)->findOneBy(['email' => $email]);
+		->getRepository(Users::class)->findOneBy(['email' => $email]);
 		
 		return $email;
 	}
@@ -136,7 +136,7 @@ class DBService{
 	public function add_user($name,$surname,$email,$password,$salt,$city,$birthdate,$gender){
 		$user = new Users();
 		$user->setname($name);
-        $user->setsurname($surname);
+		$user->setsurname($surname);
 		$user->setemail($email);
 		$user->setpassword($password);
 		$user->setsalt($salt);
--- socialweb/src/AppBundle/Service/FileService.php
+++ socialweb/src/AppBundle/Service/FileService.php
@@ -5,15 +5,15 @@ namespace AppBundle\Service;
 class FileService{
 	
 	public function save_user_image($image){
-	    $results = array();
-	    $extensions=array('jpg','jpe','jpeg','jfif','png','bmp','dib','gif');
+		$results = array();
+		$extensions=array('jpg','jpe','jpeg','jfif','png','bmp','dib','gif');
 		if(in_array(pathinfo($image['name'])['extension'], $extensions)){
-		    $tmp=$image['tmp_name'];
-	        $org=$image['name'];
-	        $name=pathinfo($tmp)['filename'].".";
-	        $name.=pathinfo($org)['extension'];
-	        if(!move_uploaded_file($tmp,'images/'.$name)){
-			    $results[0] = "Wystąpił błąd";
+			$tmp=$image['tmp_name'];
+			$org=$image['name'];
+			$name=pathinfo($tmp)['filename'].".";
+			$name.=pathinfo($org)['extension'];
+			if(!move_uploaded_file($tmp,'images/'.$name)){
+				$results[0] = "Wystąpił błąd";
 			}
 			else{
 				$results[0] = '';	
@@ -22,13 +22,13 @@ class FileService{
 			$results[1] = $name;
 		}
 		else
-		    $results[0] = "Proszę wybrać obraz";
+			$results[0] = "Proszę wybrać obraz";
 	
-	    return $results;
+		return $results;
 	}	
 	
 	public function remove_user_image($image){
-	   unlink('images/'.$image);
+		unlink('images/'.$image);
 	}
 }	
 	
